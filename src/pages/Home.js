a889d6af3d680fc8ad3afabdcc005bca62ef9c0d
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-02-29 01:30:54 -0800

    added material ui component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import HomeView from '../views/HomeView'
 
 export default function Home() {
     return (
     <div>
 
-        <p>hello</p>
+        <HomeView/>
     </div>
     )
 }
